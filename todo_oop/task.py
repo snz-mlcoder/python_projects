fe050b9ca2b0a8a1ebbc67040b5b27f5f8be8d14
Saeideh <snzart2019@gmail.com>
2025-03-24 09:52:51 +0330

    create task class

---
+++ todo_oop/task.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from matplotlib.pyplot import title
+
+
+class task:
+    def __init__(self, title , date = None):
+        self.title = title
+        self.date = datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
+
+    def to_dict(self):
+        return {
+            "title" : self.title,
+            "date"  :self.date
+        }
+    @staticmethod
+    def from_dict(data):
+        return Task(data["title"], data["date"])
