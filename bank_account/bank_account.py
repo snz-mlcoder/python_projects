60b0e0e24dcba79a80c359a6b65bf774095630c9
Saeideh <snzart2019@gmail.com>
2025-05-12 11:48:34 +0200

    Add transaction history tracking to BankAccount class

--- bank_account/bank_account.py
+++ bank_account/bank_account.py
@@ -2,13 +2,17 @@ class BankAccount:
     def __init__(self, owner, balance=0):
         self.owner = owner
         self.__balance = balance  # Private variable
+        self.__transaction = [] # private transaction list 
 
     def deposit(self, amount):
         if amount > 0:
             self.__balance += amount
+            self.__transaction.append(f"Deposited {amount}")
+
             print(f"{amount} deposited. New balance: {self.__balance}")
         else:
             print("Deposit amount must be positive.")
+
 
     def withdraw(self, amount):
         if amount > self.__balance:
@@ -17,7 +21,19 @@ class BankAccount:
             print("Withdrawal amount must be positive.")
         else:
             self.__balance -= amount
+            self.__transaction.append(f"withdraw {amount}")
             print(f"{amount} withdrawn. New balance: {self.__balance}")
+
+        
+
+    def show_transactions(self):
+        if not self.__transaction:
+            print ('No transaction yet.')
+        else:
+            print ("transaction history:")
+            for t in self.__transaction:
+                print ("-",t)
+
 
     def get_balance(self):
         return self.__balance
@@ -32,6 +48,9 @@ print(account)
 account.deposit(500)
 account.withdraw(200)
 print("Final balance:", account.get_balance())
+
+account.show_transactions()
+
 
 # Direct access to private variable (not recommended)
 # print(account.__balance)  # This will raise an AttributeError
